330ab6ce6d263c043f368bef50facceead20bac6
Hulian2000 <prepmoringa11@gmail.com>
2019-08-21 10:57:05 +0300

    add files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoalComponent } from './goal/goal.component';
+import { GoalFormComponent } from './goal-form/goal-form.component';
+import { GoalDetailsComponent } from './goal-details/goal-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalComponent,
+    GoalFormComponent,
+    GoalDetailsComponent
   ],
   imports: [
     BrowserModule,
